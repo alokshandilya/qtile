2f5eb57f61d9e1fef903b29284f3d383630eb9c7
alokshandilya <alok.shandilya10@gmail.com>
2024-08-22 16:04:25 +0530

    chore: update autostart.sh (wayland alternatives)

--- autostart.sh
+++ autostart.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# clipboard
 copyq &
 
-dunst &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-dbus-update-activation-environment --all &
-# conky -c ~/.config/conky/gruvbox-material.conkyrc &
+# desktop notification
+mako &
+
+# Outputs
+kanshi &
+
 # /usr/bin/emacs --daemon &
-nm-applet &
+
+# nm-applet
+nm-applet --indicator &
 /usr/bin/kdeconnectd &
 kdeconnect-indicator &
 
+# wallpaper
 swww-daemon &
